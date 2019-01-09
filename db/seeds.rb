3e7be903923d6f44c62cbb2ccb454350d4ac3dcb
Tobias Norton <tnorton20@yahoo.com>
2019-01-09 18:26:19 -0500

    Generated fake data in seeds.rb and some in Browse.js

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,16 +16,130 @@
 # rails db:schema:load db:seed
 
 
-gavin = Profile.create(name: "Gavin", location: "Antigua, Guatemala", about: "Bacon ipsum dolor amet pork loin tenderloin cupim leberkas sausage, burgdoggen bresaola shankle corned beef flank turducken. Ribeye pancetta beef fatback, beef ribs swine sirloin tail. Strip steak tongue shankle bacon, leberkas corned beef fatback meatball filet mignon. Boudin tenderloin swine fatback cupim pork chop tri-tip.", why_joined: Faker::PrincessBride.quote, phone: "941-911-9111", whatsapp: "+ 1 234098 123098", email: "person@email.com")
 
-gavin.trips.create(location: "Chicago", start_date: "2019-01-21", end_date: "2012-01-22" )
-gavin.trips.create(location: "New York", start_date: "2019-01-23", end_date: "2012-01-24" )
+# "name": "Jean-Sebastian Sirois",
+# "age": "",
+# "gender": "",
+# "location": "Paris, France",
+# "about": "",
+# "why_joined": "",
+# "facebook": "",
+# "instagram": "",
+# "phone": "",
+# "whatsapp": "",
+# "email": "",
+# "latitude": "",
+# "longitude": ""
 
-cities = [Faker::Coffee.origin, Faker::Coffee.origin, Faker::Coffee.origin, Faker::Coffee.origin, Faker::Coffee.origin, Faker::Coffee.origin, Faker::Coffee.origin, Faker::Coffee.origin, Faker::Coffee.origin, Faker::Coffee.origin, Faker::Coffee.origin, Faker::Coffee.origin, Faker::Coffee.origin, Faker::Coffee.origin, Faker::Coffee.origin, Faker::Coffee.origin, Faker::Coffee.origin]
+Profile.create(name: "Jean-Sebastian Sirois", age: 66, gender: "M", location: "Paris, France", about: "Bacon ipsum dolor amet prosciutto picanha kielbasa, landjaeger pig flank porchetta short loin. Short loin chicken meatloaf chuck rump beef ribs swine buffalo. Short ribs kevin shank biltong beef. Boudin flank sausage, cupim tail filet mignon bresaola turkey landjaeger salami ham meatball leberkas capicola rump.", why_joined: "Alcatra ribeye leberkas hamburger spare ribs beef, beef ribs drumstick short loin landjaeger. Ham porchetta filet mignon, turkey doner bacon ground round pork chop salami corned beef brisket pork belly ball tip chuck spare ribs.", facebook: "Jean-Sebastian Sirois", instagram: "@instagram", phone: "941-911-9111", whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude: 123, longitude: 123 )
+
+Profile.create(name: "Margaux Anati", age: 26, gender: "F", location: "Paris, France", about: "Bacon ipsum dolor amet prosciutto picanha kielbasa, landjaeger pig flank porchetta short loin. Short loin chicken meatloaf chuck rump beef ribs swine buffalo. Short ribs kevin shank biltong beef. Boudin flank sausage, cupim tail filet mignon bresaola turkey landjaeger salami ham meatball leberkas capicola rump.", why_joined: "Alcatra ribeye leberkas hamburger spare ribs beef, beef ribs drumstick short loin landjaeger. Ham porchetta filet mignon, turkey doner bacon ground round pork chop salami corned beef brisket pork belly ball tip chuck spare ribs.", facebook: "Margaux Anati", instagram:, phone: "941-911-9111", whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+Profile.create(name: "Bruno Chastain", age: 26, gender: "M", location: "Paris, France", about: "Bacon ipsum dolor amet prosciutto picanha kielbasa, landjaeger pig flank porchetta short loin. Short loin chicken meatloaf chuck rump beef ribs swine buffalo. Short ribs kevin shank biltong beef. Boudin flank sausage, cupim tail filet mignon bresaola turkey landjaeger salami ham meatball leberkas capicola rump.", why_joined: "Alcatra ribeye leberkas hamburger spare ribs beef, beef ribs drumstick short loin landjaeger. Ham porchetta filet mignon, turkey doner bacon ground round pork chop salami corned beef brisket pork belly ball tip chuck spare ribs.", facebook: "Bruno Chastain", instagram:, phone: "941-911-9111", whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+Profile.create(name: "Sarah Richelieu", age: 26, gender: "F", location: "Paris, France", about: "Bacon ipsum dolor amet prosciutto picanha kielbasa, landjaeger pig flank porchetta short loin. Short loin chicken meatloaf chuck rump beef ribs swine buffalo. Short ribs kevin shank biltong beef. Boudin flank sausage, cupim tail filet mignon bresaola turkey landjaeger salami ham meatball leberkas capicola rump.", why_joined: "Alcatra ribeye leberkas hamburger spare ribs beef, beef ribs drumstick short loin landjaeger. Ham porchetta filet mignon, turkey doner bacon ground round pork chop salami corned beef brisket pork belly ball tip chuck spare ribs.", facebook: "Sarah Richelieu", instagram:, phone: "941-911-9111", whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+Profile.create(name: "Duanphen Kaekwoon", age: 26, gender: "F", location: "Paris, France", about: "Chiang Mai, Thailand", about: "Bacon ipsum dolor amet prosciutto picanha kielbasa, landjaeger pig flank porchetta short loin. Short loin chicken meatloaf chuck rump beef ribs swine buffalo. Short ribs kevin shank biltong beef. Boudin flank sausage, cupim tail filet mignon bresaola turkey landjaeger salami ham meatball leberkas capicola rump.", why_joined: "Alcatra ribeye leberkas hamburger spare ribs beef, beef ribs drumstick short loin landjaeger. Ham porchetta filet mignon, turkey doner bacon ground round pork chop salami corned beef brisket pork belly ball tip chuck spare ribs.", facebook: "Hector Darwin", instagram:, phone: "941-911-9111", whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+Profile.create(name: "Budsarin Hiranprueck", age: 26, gender: "M", location: "Paris, France", about: "Chiang Mai, Thailand", about: "Bacon ipsum dolor amet prosciutto picanha kielbasa, landjaeger pig flank porchetta short loin. Short loin chicken meatloaf chuck rump beef ribs swine buffalo. Short ribs kevin shank biltong beef. Boudin flank sausage, cupim tail filet mignon bresaola turkey landjaeger salami ham meatball leberkas capicola rump.", why_joined: "Alcatra ribeye leberkas hamburger spare ribs beef, beef ribs drumstick short loin landjaeger. Ham porchetta filet mignon, turkey doner bacon ground round pork chop salami corned beef brisket pork belly ball tip chuck spare ribs.", facebook:, instagram:, phone: "941-911-9111", whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+Profile.create(name: "Naowarat Angsakul", age: 26, gender: "M", location: "Chiang Mai, Thailand", about: "Bacon ipsum dolor amet prosciutto picanha kielbasa, landjaeger pig flank porchetta short loin. Short loin chicken meatloaf chuck rump beef ribs swine buffalo. Short ribs kevin shank biltong beef. Boudin flank sausage, cupim tail filet mignon bresaola turkey landjaeger salami ham meatball leberkas capicola rump.", why_joined: "Alcatra ribeye leberkas hamburger spare ribs beef, beef ribs drumstick short loin landjaeger. Ham porchetta filet mignon, turkey doner bacon ground round pork chop salami corned beef brisket pork belly ball tip chuck spare ribs.", facebook:, instagram:, phone: "941-911-9111", whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+Profile.create(name: "Michael Kenny", age: 26, gender: "M", location: "Galway, Ireland", about: "Galway, Ireland", about: "Bacon ipsum dolor amet prosciutto picanha kielbasa, landjaeger pig flank porchetta short loin. Short loin chicken meatloaf chuck rump beef ribs swine buffalo. Short ribs kevin shank biltong beef. Boudin flank sausage, cupim tail filet mignon bresaola turkey landjaeger salami ham meatball leberkas capicola rump.", why_joined: "Alcatra ribeye leberkas hamburger spare ribs beef, beef ribs drumstick short loin landjaeger. Ham porchetta filet mignon, turkey doner bacon ground round pork chop salami corned beef brisket pork belly ball tip chuck spare ribs.", facebook:, instagram:, phone: "941-911-9111", whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+Profile.create(name: "Larry McCulloch", age: 26, gender: "M", location: "Galway, Ireland", about: "Bacon ipsum dolor amet prosciutto picanha kielbasa, landjaeger pig flank porchetta short loin. Short loin chicken meatloaf chuck rump beef ribs swine buffalo. Short ribs kevin shank biltong beef. Boudin flank sausage, cupim tail filet mignon bresaola turkey landjaeger salami ham meatball leberkas capicola rump.", why_joined: "Alcatra ribeye leberkas hamburger spare ribs beef, beef ribs drumstick short loin landjaeger. Ham porchetta filet mignon, turkey doner bacon ground round pork chop salami corned beef brisket pork belly ball tip chuck spare ribs.", facebook:, instagram:, phone: "941-911-9111", whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+Profile.create(name: "Kenny Matthews", age: 26, gender: "M", location: "Galway, Ireland", about: "Bacon ipsum dolor amet prosciutto picanha kielbasa, landjaeger pig flank porchetta short loin. Short loin chicken meatloaf chuck rump beef ribs swine buffalo. Short ribs kevin shank biltong beef. Boudin flank sausage, cupim tail filet mignon bresaola turkey landjaeger salami ham meatball leberkas capicola rump.", why_joined: "Alcatra ribeye leberkas hamburger spare ribs beef, beef ribs drumstick short loin landjaeger. Ham porchetta filet mignon, turkey doner bacon ground round pork chop salami corned beef brisket pork belly ball tip chuck spare ribs.", facebook:, instagram:, phone: "941-911-9111", whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+Profile.create(name: "Todd Miller", age: 26, gender: "M", location: "Galway, Ireland", about: "Bacon ipsum dolor amet prosciutto picanha kielbasa, landjaeger pig flank porchetta short loin. Short loin chicken meatloaf chuck rump beef ribs swine buffalo. Short ribs kevin shank biltong beef. Boudin flank sausage, cupim tail filet mignon bresaola turkey landjaeger salami ham meatball leberkas capicola rump.", why_joined: "Alcatra ribeye leberkas hamburger spare ribs beef, beef ribs drumstick short loin landjaeger. Ham porchetta filet mignon, turkey doner bacon ground round pork chop salami corned beef brisket pork belly ball tip chuck spare ribs.", facebook:, instagram:, phone: "941-911-9111", whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+# Profile.create(name: "Jean-Sebastian Sirois", age: 26, gender: "M", location:, about:, why_joined:, facebook:, instagram:, phone:, whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+# Profile.create(name: "Jean-Sebastian Sirois", age: 26, gender: "M", location:, about:, why_joined:, facebook:, instagram:, phone:, whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+# Profile.create(name: "Jean-Sebastian Sirois", age: 26, gender: "M", location:, about:, why_joined:, facebook:, instagram:, phone:, whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+# Profile.create(name: "Jean-Sebastian Sirois", age: 26, gender: "M", location:, about:, why_joined:, facebook:, instagram:, phone:, whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+# Profile.create(name: "Jean-Sebastian Sirois", age: 26, gender: "M", location:, about:, why_joined:, facebook:, instagram:, phone:, whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+# Profile.create(name: "Jean-Sebastian Sirois", age: 26, gender: "M", location:, about:, why_joined:, facebook:, instagram:, phone:, whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+# Profile.create(name: "Jean-Sebastian Sirois", age: 26, gender: "M", location:, about:, why_joined:, facebook:, instagram:, phone:, whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+# Profile.create(name: "Jean-Sebastian Sirois", age: 26, gender: "M", location:, about:, why_joined:, facebook:, instagram:, phone:, whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+# Profile.create(name: "Jean-Sebastian Sirois", age: 26, gender: "M", location:, about:, why_joined:, facebook:, instagram:, phone:, whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+# Profile.create(name: "Jean-Sebastian Sirois", age: 26, gender: "M", location:, about:, why_joined:, facebook:, instagram:, phone:, whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+# Profile.create(name: "Jean-Sebastian Sirois", age: 26, gender: "M", location:, about:, why_joined:, facebook:, instagram:, phone:, whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+
+# Profile.create(name: "Jean-Sebastian Sirois", age: 26, gender: "M", location:, about:, why_joined:, facebook:, instagram:, phone:, whatsapp: "+ 1 234098 123098", email: "person@email.com", latitude:, longitude: )
+# {
+#     "name": "Jean-Sebastian Sirois",
+#     "location": "Paris, France",
+#     "availability": "June 18 - July 8, 2019",
+#     "image": "./JeanSebastian.jpeg"
+#   },
+#   {
+#     "name": "Hector Darwin",
+#     "location": "Paris, France",
+#     "availability": "June 18 - July 8, 2019",
+#     "image": "./Hector.jpg"
+#   }
+#   {
+#     "name": "Margaux Anati",
+#     "location": "Paris, France",
+#     "availability": "June 18 - July 8, 2019",
+#     "image": "./Margaux.jpg"
+#   },
+#   {
+#     "name": "Bruno Chastain",
+#     "location": "Paris, France",
+#     "availability": "June 18 - July 8, 2019",
+#     "image": "./Bruno.jpg"
+#   },
+#   {
+#     "name": "Sarah Richelieu",
+#     "location": "Paris, France",
+#     "availability": "June 18 - July 8, 2019",
+#     "image": "./Sarah.jpg"
+#   },
+#   {
+#     "name": "Duanphen Kaekwoon",
+#     "location": "Chiang Mai, Thailand",
+#     "availability": "July 9 - July 24, 2019",
+#     "image": "./Duanphen.jpeg"
+#   },
+#   {
+#     "name": "Budsarin Hiranprueck",
+#     "location": "Chiang Mai, Thailand",
+#     "availability": "July 9 - July 24, 2019",
+#     "image": "./BudsarinCropped.jpg"
+#   },
+#   {
+#     "name": "Naowarat Angsakul",
+#     "location": "Chiang Mai, Thailand",
+#     "availability": "July 9 - July 24, 2019",
+#     "image": "./Naowarat.jpg"
+#   }
+
+
+# gavin = Profile.create(name: "Gavin", location: "Antigua, Guatemala", about: "Bacon ipsum dolor amet pork loin tenderloin cupim leberkas sausage, burgdoggen bresaola shankle corned beef flank turducken. Ribeye pancetta beef fatback, beef ribs swine sirloin tail. Strip steak tongue shankle bacon, leberkas corned beef fatback meatball filet mignon. Boudin tenderloin swine fatback cupim pork chop tri-tip.", why_joined: Faker::PrincessBride.quote, phone: "941-911-9111", whatsapp: "+ 1 234098 123098" "+ 1 234098 123098", email: "person@email.com")
+
+# gavin.trips.create(location: "Chicago", start_date: "2019-01-21", end_date: "2012-01-22" )
+# gavin.trips.create(location: "New York", start_date: "2019-01-23", end_date: "2012-01-24" )
+
+cities = ["Galway, Ireland", "Paris, France", "Tangier, Morocco", ]
 
 genders = ["M", "F", "Other/Prefer not to answer"]
 
-100.times do
-  fake_person = Profile.create(name: Faker::Name.name, age: rand(18..68), gender: genders.sample, location: cities.sample, about: Faker::PrincessBride.quote, why_joined: Faker::PrincessBride.quote, phone: "941-911-9111", whatsapp: "+ 1 234098 123098", email: "person@email.com")
+20.times do
+  fake_person = Profile.create(name: Faker::Name.name, age: rand(18..68), gender: genders.sample, location: cities.sample, about: Faker::PrincessBride.quote, why_joined: Faker::PrincessBride.quote, phone: "941-911-9111", whatsapp: "+ 1 234098 123098" "+ 1 234098 123098", email: "person@email.com")
+
   fake_person.trips.create(location: cities.sample, start_date: Faker::Date.between(12.days.from_now, Date.today), end_date: Faker::Date.between(Date.today, 100.days.from_now))
+
+  # fake_person.link
 end
+
